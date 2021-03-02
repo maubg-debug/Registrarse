e1b2c8dd0ffd5f9d6a9758e7ee31706f0dfa831c
Maubg <68229077+maubg-debug@users.noreply.github.com>
2021-03-02 23:43:05 +0100

    Update controllerUserData.php

--- sigup/controllerUserData.php
+++ sigup/controllerUserData.php
@@ -11,12 +11,12 @@
         $password = mysqli_real_escape_string($con, $_POST['password']);
         $cpassword = mysqli_real_escape_string($con, $_POST['cpassword']);
         if($password !== $cpassword){
-            $errors['password'] = "Confirm password not matched!";
+            $errors['password'] = "Contraseñas no coinciden!";
         }
         $email_check = "SELECT * FROM usertable WHERE email = '$email'";
         $res = mysqli_query($con, $email_check);
         if(mysqli_num_rows($res) > 0){
-            $errors['email'] = "Email that you have entered is already exist!";
+            $errors['email'] = "Ese email ya existe!";
         }
         if(count($errors) === 0){
             $encpass = password_hash($password, PASSWORD_BCRYPT);
@@ -26,19 +26,19 @@
                             values('$name', '$email', '$encpass', '$code', '$status')";
             $data_check = mysqli_query($con, $insert_data);
             if($data_check){
-                $subject = "Email Verification Code";
-                $message = "Your verification code is $code";
-                $sender = "From: shahiprem7890@gmail.com";
+                $subject = "Codigo de verificacion";
+                $message = "Tu codigo de verificacion es $code";
+                $sender = "From: "; // Email
                 if(mail($email, $subject, $message, $sender)){
-                    $info = "We've sent a verification code to your email - $email";
+                    $info = "Te hemos enviado un codigo de verificacion - $email";
                     $_SESSION['info'] = $info;
                     header('location: user-otp.php');
                     exit();
                 }else{
-                    $errors['otp-error'] = "Failed while sending code!";
+                    $errors['otp-error'] = "Un error intentando enviar email!";
                 }
             }else{
-                $errors['db-error'] = "Failed while inserting data into database!";
+                $errors['db-error'] = "Error intentando entrar en la base de datos!";
             }
         }
 
@@ -63,10 +63,10 @@
                         header('location: home.php');
                         exit();
                     }else{
-                        $errors['otp-error'] = "Failed while updating code!";
+                        $errors['otp-error'] = "Fallado intentando actualizar tu codigo!";
                     }
                 }else{
-                    $errors['otp-error'] = "You've entered incorrect code!";
+                    $errors['otp-error'] = "Codigo incorecto!";
                 }
         }
 
@@ -85,7 +85,7 @@
                     $_SESSION['email'] = $email;
                         header('location: home.php');
                     }else{
-                        $info = "It's look like you haven't still verify your email - $email";
+                        $info = "Aun no as verificado tu email - $email";
                         $_SESSION['info'] = $info;
                         header('location: user-otp.php');
                     }
@@ -93,6 +93,6 @@
                     $errors['email'] = "Incorrect email or password!";
                 }
             }else{
-                $errors['email'] = "It's look like you're not a yet member! Click on the bottom link to signup.";
+                $errors['email'] = "Aun no eres un miembro! Haz click en el boton para crearte una cuenta.";
             }
         }
